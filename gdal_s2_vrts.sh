451b095adf78026538bde321ecbfc1c9d22f75be
RNanclares <raulnanclares@gmail.com>
2021-10-21 15:44:35 -0500

    added bash script for automatic sentinel 2 vrt creation

---
+++ gdal_s2_vrts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for imagen in *.zip; do
+  echo "$imagen"
+  #vzip = '/vsizip//'
+  unzip -Z -1 $imagen | grep -E B.._10m.jp2$ | sort | sed -e 's\^\/vsizip//'"$PWD/$(basename $imagen)"'/\' > bandList.txt
+  gdalbuildvrt -resolution average -separate -r nearest -input_file_list bandList.txt "$(basename "$imagen" .zip)"_10m.vrt
+  unzip -Z -1 $imagen | grep -E B0._10m.jp2$ | sort | sed -e 's\^\/vsizip//'"$PWD/$(basename $imagen)"'/\' > bandList20m.txt
+  unzip -Z -1 $imagen | grep -E B8A_10m.jp2$ | sort | sed -e 's\^\/vsizip//'"$PWD/$(basename $imagen)"'/\' >> bandList20m.txt
+  unzip -Z -1 $imagen | grep -E B1._20m.jp2$ | sort | sed -e 's\^\/vsizip//'"$PWD/$(basename $imagen)"'/\' >> bandList20m.txt
+  gdalbuildvrt -resolution average -separate -r nearest -input_file_list bandList20m.txt "$(basename "$imagen" .zip)"_20m.vrt
+  rm bandList*
+done
